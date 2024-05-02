de438e6c4c684b3d95366fec9d63f1e7fb059425
anggaSaputra5 <anggasaputra2st@gmail.com>
2024-05-02 11:20:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Portfolio
+
+Ayo kenalan lebih jauh dengan cara melihat portfolio saya! Saya membuatnya dengan framework bootstrap dan beberapa tambahan CSS dan JS, jangan lupa untuk follow saya disemua sosial media saya!
+
+<p align="center"><img src="ss.png" width="100%" alt="Screenshot Home"></p>
---
+++ script.js
@@ -0,0 +1,35 @@
+// Animate On Scrolling
+AOS.init();
+// Fancybox
+Fancybox.bind("[data-fancybox]", {
+    on: {
+        "*": (fancybox, eventName) => {
+        console.log(`Fancybox eventName: ${eventName}`);
+        },
+    },
+});
+// Remove Overflow
+window.addEventListener('DOMContentLoaded', function() {
+    var homeDiv = document.getElementById('home');
+    var aboutDiv = document.getElementById('about');
+    var projectsDiv = document.getElementById('projects');
+    var contactDiv = document.getElementById('contact');
+    var body = document.body;
+
+    // Cek apakah tinggi div home melebihi 100vh
+    if (homeDiv.clientHeight > window.innerHeight || aboutDiv.clientHeight > window.innerHeight || projectsDiv.clientHeight > window.innerHeight || contactDiv.clientHeight > window.innerHeight) {
+        body.classList.remove('overflow-hidden');
+    }
+});
+
+// Send message
+function sendWhatsApp() {
+    const nama = document.getElementById("nama").value;
+    const email = document.getElementById("email").value;
+    const pesan = document.getElementById("pesan").value;
+
+    var message = "Nama : " + nama + "\nEmail : " + email + "\nPesan : " + pesan;
+
+    var whatsappLink = "https://wa.me/6283847406524?text=" + encodeURIComponent(message);
+    window.open(whatsappLink, "_blank");
+}
